7acd15d3422998687ef7cf345104f42e3e3a0d28
Bence Turák <bence.turak@gmail.com>
2021-04-06 17:23:08 +0200

    RNXReader logged and commented

--- rnxreader.py
+++ rnxreader.py
@@ -1,47 +1,62 @@
 import numpy as np
 import math
+import logging
+from epoch import Epoch
 
 class RNXReader(object):
+    """
+        RNXReader class to read RINEX observation file
+        RINEX v2.10
+
+            :param fileName: name of RINEX file (string)
+
+    """
 
     def __init__(self, fileName):
+        """RNXReader constructor
 
-        self.fileName = fileName
-        self.comments = []
-        self.observationTypes = []
-        self.numberOfObservationTypes = 0
-        self.observations = {}
+        """
+        self.fileName = fileName#filname
+        self.comments = []#comment records
+        self.observationTypes = []#list of observation types
+        self.numberOfObservationTypes = 0#number of observation types
+        self.observations = {}#observations
         try:
             self.fid = open(self.fileName, 'r')
-
+            #start read of header
             self._readHeader()
-
-        finally:
-            self.fid.close()
+        except IOError as e:
+            #handle open file error
+            logging.error(e)
 
     def getValidTimeFrame(self):
         pass
 
     def readObservations(self):
+        """read observations from RINEX file
+
+        """
         line = self.fid.readline()
 
-        i = 1
+        #read observation file row by row
         while line:
-            i = i + 1
-            #print(line)
+            #read next epoch
             self._readEpochSats(line)
             line = self.fid.readline()
 
 
     def _readEpochSats(self, line):
+        """read satellites at epoch
+                :param line: line from RINEX file (string)
+        """
 
         listOfSats = []
-
+        #read epoch
+        #read 2 digits year and convert to 4 digits
         if int(line[1:3]) > 80:
             year = 1900 + int(line[1:3])
         else:
             year = 2000 + int(line[1:3])
-
-        month = int(line[1:3])
 
         month = int(line[4:6])
         day = int(line[7:9])
@@ -49,107 +64,123 @@ class RNXReader(object):
         min = int(line[13:15])
         sec = float(line[15:26])
 
-        epoch = np.array([[year, month, day, hour, min, sec]])
+        #epoch
+        epoch = Epoch(np.array([year, month, day, hour, min, sec]))
 
+        #epoch state flag
         epochFlag = int(line[28:29])
 
+        #number of satellites in this epoch
         numOfSats = int(line[29:32])
 
 
-        rows = int((numOfSats-1)/12)
+        rows = int((numOfSats-1)/12)#number of full(12 satellites) rows
+        resid = numOfSats%12#residual satellites in last row
 
-
-        resid = numOfSats%12
-
+        #if residual is 0, then the row is full
         if resid == 0:
             resid = 12
 
-
-        for j in range(rows):
+        #read the satellites whats are observed in this epoch
+        for j in range(rows):#full rows
             [listOfSats.append(line[32+i*3:35+i*3].replace(" ", "0")) for i in range(12)]
             line = self.fid.readline()
 
-        [listOfSats.append(line[32+i*3:35+i*3].replace(" ", "0")) for i in range(resid)]
+        [listOfSats.append(line[32+i*3:35+i*3].replace(" ", "0")) for i in range(resid)]#residual
 
+        #read observation sat by sat
         for i in range(numOfSats):
             sat = listOfSats[i]
 
 
-            if sat in self.observations.keys():
+            if sat in self.observations.keys():#if already there is at least one observation to this satellite
                 self.observations[sat] = np.append(self.observations[sat], self._readObs(epoch), axis=0)
-            else:
+            else:#if there is not observation to this satellite
                 self.observations[sat] = self._readObs(epoch)
 
 
 
     def _readObs(self, epoch):
+        """read observations by satellites of epoch
+                :epoch: epoch of observations(Epoch)
+        """
+        #return observation in epoch
+        re_epoch = np.array([[epoch]])
+        #number of full rows to one sat
         obsRows = int(self.numberOfObservationTypes / 5)
+        #number of observations in the residual row
         obsResid = self.numberOfObservationTypes % 5
 
-
-        for i in range(obsRows):
+        #read observations row by row
+        for i in range(obsRows):#full rows
             line = self.fid.readline()
-            for j in range(5):
+            for j in range(5):#step on every observation blocks
                 block = line[0+j*16:16+j*16]
-
-                if block[0:14].strip() == '':
+                #observation
+                if block[0:14].strip() == '':#if empty the vlue is 0
                     obs = 0
                 else:
                     obs = float(block[0:14])
-
-                if block[14:15].strip() == '':
+                #LLI
+                if block[14:15].strip() == '':#if empty the vlue is 0
                     LLI = 0
                 else:
                     LLI = int(block[14:15])
-
-                if block[15:16].strip() == '':
+                #signal strength
+                if block[15:16].strip() == '':#if empty the vlue is 0
                     sigStr = 0
                 else:
                     sigStr = int(block[15:16])
 
-                epoch = np.append(epoch, [[obs, LLI, sigStr]], axis=1)
+                re_epoch = np.append(re_epoch, [[obs, LLI, sigStr]], axis=1)
 
+        #residual row
         line = self.fid.readline()
-        for j in range(obsResid):
+        for j in range(obsResid):#step on every observation blocks
             block = line[0+j*16:16+j*16]
 
-
-            if block[0:14].strip() == '':
+            #observation
+            if block[0:14].strip() == '':#if empty the vlue is 0
                 obs = 0
             else:
                 obs = float(block[0:14])
-
-            if block[14:15].strip() == '':
+            #LLI
+            if block[14:15].strip() == '':#if empty the vlue is 0
                 LLI = 0
             else:
                 LLI = int(block[14:15])
-
-            if block[15:16].strip() == '':
+            #signal strength
+            if block[15:16].strip() == '':#if empty the vlue is 0
                 sigStr = 0
             else:
                 sigStr = int(block[15:16])
 
-            epoch = np.append(epoch, [[obs, LLI, sigStr]], axis=1)
+            re_epoch = np.append(re_epoch, [[obs, LLI, sigStr]], axis=1)
 
-        return epoch
+        return re_epoch
 
 
 
     def _readHeader(self):
+        """read RINEX header v2.10
 
+        """
+        #read RINEX file row by row
         while True:
 
-            line = self.fid.readline()
+            line = self.fid.readline()#read a row
 
-            type = line[60:].replace("/","_").replace(":","_").replace("#","").replace(" ","").replace("\n","")
+            type = line[60:].replace("/","_").replace(":","_").replace("#","").replace(" ","").replace("\n","")#replace special chars in title
 
 
             try:
-                print("self."+type+"(line)")
+                #read header record
                 eval("self."+type+"(line)")
+                logging.info(line[60:].strip() + " record is readed")
             except AttributeError as er:
-                print(er)
+                logging.warning(line[60:].strip() + " cannot be readed")
 
+            #break loop when header is ended
             if type == 'ENDOFHEADER':
                 break
 
@@ -227,7 +258,7 @@ class RNXReader(object):
         min = int(line[24:30])
         sec = float(line[30:43])
 
-        self.timeOfFristObs = np.array([year, month, day, hour, min, sec])
+        self.timeOfFristObs = Epoch(np.array([year, month, day, hour, min, sec]))
         self.timeSystem = line[48:51].strip()
 
     def TIMEOFLASTOBS(self, line):
@@ -238,7 +269,7 @@ class RNXReader(object):
         min = int(line[24:30])
         sec = float(line[30:43])
 
-        self.timeOfLastObs = np.array([year, month, day, hour, min, sec])
+        self.timeOfLastObs = Epoch(np.array([year, month, day, hour, min, sec]))
         #self.timeSystem = line[48:51].strip()
 
 
@@ -247,8 +278,17 @@ class RNXReader(object):
         pass
 
     def getObservations(self, sat, obsType, opt=0):
+        """get observation by satellite PRN and observation type
+
+                :sat: PRN 3-digits system-char [GPS: G,GLONASS: R, Galileo: E (RINEX standards)] and PRN extended with 0 if necessary(string)
+                :obsType: observation type, marked as in RINEX standard (string)
+                :opt: observation optionally can be extended obs informations [LLI, signal strenth] (int 0 or 1), default:0
+
+        """
+        #key of obervation type
         key = self.observationTypes.index(obsType)
-        return np.append(self.observations[sat][:,0:6], self.observations[sat][:,6+key*3:6+key*3+1+2*opt], axis=1)
+
+        return np.append(self.observations[sat][:,0:1], self.observations[sat][:,1+key*3:1+key*3+1+2*opt], axis=1)
 
 
 
