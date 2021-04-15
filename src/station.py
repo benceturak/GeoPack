b07d25c65fdfeb18b4d5423d5bf484b9feb1815d
Bence Turák <bence.turak@gmail.com>
2021-04-15 08:05:05 +0200

    init Station class

---
+++ src/station.py
@@ -0,0 +1,11 @@
+import numpy as np
+from point import Point
+
+class Station(Point):
+
+    def __init__(self, id = '0', coord = np.array([[0.0],[0.0],[0.0]])):
+
+
+
+    def __repr__(self):
+        return 0#self.coord
