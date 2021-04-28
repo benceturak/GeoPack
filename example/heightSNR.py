3b464a23886b8f03fb406045efae46b160bdd780
Bence Turák <bence.turak@gmail.com>
2021-04-28 18:13:18 +0200

    derived antenna height with lomb-scargle

--- example/heightSNR.py
+++ example/heightSNR.py
@@ -11,6 +11,8 @@ from wgs84 import WGS84
 from point import Point
 import matplotlib
 import matplotlib.pyplot as plt
+import scipy.signal as signal
+from lsfrequency import lsfrequency
 
 
 obs = RNXReader('../data/118/mini.19o')
@@ -34,13 +36,25 @@ for i in SNR17:
         SNR = np.append(SNR, [a], axis=0)
     except TimeError:
         pass
-
-
 sine = np.sin(SNR[:,1])
 SNR[:,1] = 10**(SNR[:,0]/20)
-fig, ax = plt.subplots()
-ax.plot(sine, SNR[:,0])
-ax.set(xlabel="sin(e) [-]", ylabel='SNR [volts/volts]', title='SNR values G17')
-ax.grid()
+
+f = lsfrequency(s.l1, 5, 30, 0.01)
+
+p = np.poly1d(np.polyfit(sine, SNR[:,0], 2))
+
+trend = p(sine)
+
+residuals = SNR[:,0] - trend
+
+periodogram = signal.lombscargle(sine, residuals, f)
+
+fig, axs = plt.subplots(2, 1)
+axs[0].plot(sine, SNR[:,0], '-', sine, trend, '--')
+axs[0].set(xlabel="sin(e) [-]", ylabel='SNR [volts/volts]', title='SNR values G25')
+
+axs[1].plot(f*s.l1/(4*math.pi), periodogram)
+axs[1].set(xlabel="f", ylabel='aaaa', title='Periodogram values G25')
+
 
 plt.show()
