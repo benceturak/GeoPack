4ebb95609665c7223211b92521a27cc2e16a5e3d
Bence Turák <bence.turak@gmail.com>
2021-05-28 18:41:49 +0200

    all satellite validation

--- example/validator.py
+++ example/validator.py
@@ -13,10 +13,11 @@ glo = glonassnavreader.GLONASSNavReader('../data/validation/202194/abmf0940.21g'
 sp3 = sp3reader.SP3Reader('../data/validation/202194/cod21520.eph_r')
 diff = {}
 for prn in sp3.positions:
-    print(prn)
+
     diff[prn] = np.empty((0,4))
     #print(sp3.positions[prn])
     if prn[0] == 'G':
+        continue
         sat = gps.getSatellite(prn)
     elif prn[0] == 'R':
         sat = glo.getSatellite(prn)
@@ -27,8 +28,9 @@ for prn in sp3.positions:
 
             diff[prn] = np.append(diff[prn], np.append([[e[0]]], e[1:4] - sat.getSatPos(e[0]).getXYZ().T, axis=1), axis=0)
         except:
-            print('!')
-        print('---------------------------')
+            pass
+            #print('!')
+    print(prn)
     print(diff[prn])
+    print('---------------------------')
     #print(sat.getSatPos(epoch.Epoch(np.array([2021,4,3,23,59,59.0]))))
-    break
