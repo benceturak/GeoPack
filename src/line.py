a4551e66efeb69865067c3f3369c5dfd74a4b4cc
Bence Turák <bence.turak@gmail.com>
2021-07-19 18:59:35 +0200

    Line class formulas fixed

--- src/line.py
+++ src/line.py
@@ -6,13 +6,13 @@ class Line(object):
     def __init__(self, p, alpha, e):
 
         xyz = p.getXYZ()
-        self.x = lambda t: xyz[0,0] + t*np.sin(e)
-        self.y = lambda t: xyz[1,0] + t*np.cos(alpha)
-        self.z = lambda t: xyz[2,0] + t*np.sin(alpha)
+        self.x = lambda t: xyz[0,0] + t*np.cos(alpha)*np.cos(e)
+        self.y = lambda t: xyz[1,0] + t*np.sin(alpha)*np.cos(e)
+        self.z = lambda t: xyz[2,0] + t*np.sin(e)
 
-        self.xr = lambda x: (x - xyz[0,0])/np.sin(e)
-        self.yr = lambda y: (y - xyz[1,0])/np.cos(alpha)
-        self.zr = lambda z: (z - xyz[2,0])/np.sin(alpha)
+        self.xr = lambda x: (x - xyz[0,0])/(np.cos(alpha)*np.cos(e))
+        self.yr = lambda y: (y - xyz[1,0])/(np.sin(alpha)*np.cos(e))
+        self.zr = lambda z: (z - xyz[2,0])/np.sin(e)
 
     def getPointAtT(self, t):
         for tt in t:
