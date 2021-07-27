b4cee4e963e6ab84d3184223281cbc481b736f7c
Bence Turák <bence.turak@gmail.com>
2021-07-27 16:20:21 +0200

    MART fixed

--- apps/tomography/mart.py
+++ apps/tomography/mart.py
@@ -3,28 +3,22 @@ import numpy as np
 def mart(A, b, maxIter, x0, tol):
 
     x = x0
-    for k in range(1, maxIter):
-        print(k)
+    for k in range(0, maxIter):
         x1 = x
         iter = k
 
         for i in range(0, np.shape(A)[0]-1):
             theta = A[i,:]/np.linalg.norm(A[i,:])
 
-
-
             ratio = np.float_power((b[i]/np.dot(A[i,:], x)),theta).T
-            if b[i] == 0:
-                exit()
-
 
             x = x*ratio
+            err = np.abs((x - x1)/x)
         x2 = x
-        
 
         err = np.abs((x2 - x1)/x2)
+        print(k)
         print(np.max(err))
-        print('------------------------')
         if np.max(err) < tol:
             break
     ret = {}
