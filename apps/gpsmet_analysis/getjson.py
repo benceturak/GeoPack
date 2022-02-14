674472659afb8960495236013aec4303d8514178
Bence Turák <bence.turak@gmail.com>
2022-02-14 08:16:47 +0100

    get tropospheric delay json

---
+++ apps/gpsmet_analysis/getjson.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+try:
+    import sys
+    import traceback
+
+    sys.path.append("/home/bence/data/GeoPack/src")
+    from readdb import ReadDB
+    from epoch import Epoch
+    import dbconfig
+    import numpy as np
+    import getopt
+    import importlib
+    opts, args = getopt.getopt(sys.argv[1:], 's:f:t:o:h:', ['stations=', 'from=', 'to=', 'output=', 'help'])
+    stations = None
+
+
+
+    for o, v in opts:
+        if o == '--stations' or o == '-s':
+            stations = v.split(',')
+        elif o == '--from' or o == '-f':
+            dt = v.split('-')
+
+            fr = Epoch(np.array([int(dt[0]),int(dt[1]),int(dt[2]),int(dt[3]),int(dt[4]),int(dt[5])]))
+        elif o == '--to' or o == '-t':
+            dt = v.split('-')
+
+            to = Epoch(np.array([int(dt[0]),int(dt[1]),int(dt[2]),int(dt[3]),int(dt[4]),int(dt[5])]))
+        elif o == '--output' or o == '-o':
+            filename = v
+
+        elif  o == '--help' or o == '-h':
+            print("Usage:")
+
+
+    database = ReadDB(database=dbconfig.database)
+    
+
+
+    #fr = Epoch(np.array([2021,11,1,2,0,0]))
+    #to = Epoch(np.array([2021,11,1,3,0,0]))
+
+    for i in database.getZTD(stations=stations, fr=fr, to=to):
+        try:
+            p = database.getStation(i[0])
+            plh = p.getPLH()[:,0]
+            sta = Little_RStation(lat=plh[0], lon=plh[1], alt=plh[2], id=p.id, name=p.id, source="BUTE GNSS-meteorology nrt procession", epoch=i[1])
+            sta.setZTD(i[2])
+            writer.addStation(sta)
+        except ValueError as er:
+            print(er)
+
+    writer.write()
+
+except Exception as er:
+    exc_type, exc_value, exc_traceback = sys.exc_info()
