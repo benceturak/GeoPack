ca098f1e0efe7f2a76fcb1ac9608ad852ca1a3e9
Bence Turák <bence.turak@gmail.com>
2022-12-02 17:59:29 +0100

    refractivity littleR contain dry part

--- apps/gpsmet_analysis/getRefr3DLittle_R.py
+++ apps/gpsmet_analysis/getRefr3DLittle_R.py
@@ -38,74 +38,68 @@ try:
 
     database = ReadDB(database=dbconfig.database)
 
-    gridp = np.loadtxt(gridp_file)#*np.pi/180#np.arange(min[0], max[0], 0.6*np.pi/180)
-    gridl = np.loadtxt(gridl_file)#*np.pi/180#np.arange(min[1], max[1], 1.449*np.pi/180)
-    gridh = np.loadtxt(gridh_file)
-
-    gridp_center = []
-    gridl_center = []
-    gridh_center = []
-    for i in range(0, len(gridp)-1):
-        gridp_center.append((gridp[i] + gridp[i+1])/2)
-
-    for i in range(0, len(gridl)-1):
-        gridl_center.append((gridl[i] + gridl[i+1])/2)
-
-    for i in range(0, len(gridh)-1):
-        gridh_center.append((gridh[i] + gridh[i+1])/2)
-
-
-
-    #fr = Epoch(np.array([2021,11,1,2,0,0]))
-    #to = Epoch(np.array([2021,11,1,3,0,0]))
-    writer = Little_RWriter(filename)
-    RAOB_stations = []
-    coords = np.empty((0,2))
-    temp_at_coords_layers = np.empty((0, len(gridh_center)))
-    press_at_coords_layers = np.empty((0, len(gridh_center)))
-
-    for station in database.getStations("RAOB"):
-        RAOB_stations.append(station)
-
-        last_available = database.getLastRAOBSep(station.id, ep)
-
-        temp_profile = database.getRAOBSTempAtep(station_id=station.id, ep=last_available)
-        press_profile = database.getRAOBSPressAtep(station_id=station.id, ep=last_available)
-        
-        temp_layered_profile = np.append([gridh_center], [np.interp(gridh_center, temp_profile[:,0], temp_profile[:,1])], axis=0).T
-        press_layered_profile = np.append([gridh_center], [np.interp(gridh_center, press_profile[:,0], press_profile[:,1])], axis=0).T
-
-        coords = np.append(coords, station.getPLH()[0:2,:].T, axis=0)
-        temp_at_coords_layer = np.empty((len(gridh_center), ))
-        press_at_coords_layer = np.empty((len(gridh_center), ))
-        for i in range(0, len(gridh_center)):
-            temp_at_coords_layer[i] = temp_layered_profile[i,1]
-            press_at_coords_layer[i] = press_layered_profile[i,1]
-
-        temp_at_coords_layers = np.append(temp_at_coords_layers, [temp_at_coords_layer], axis=0)
-        press_at_coords_layers = np.append(press_at_coords_layers, [press_at_coords_layer], axis=0)
-
-    interp_gridp, interp_gridl =  np.meshgrid(gridp_center, gridl_center)
-    temp_model_3d = np.empty((len(gridp_center), len(gridl_center), len(gridh_center)))
-    press_model_3d = np.empty((len(gridp_center), len(gridl_center), len(gridh_center)))
-
-    for i in range(0, np.shape(temp_at_coords_layers)[1]):
-        #print("____________________________")
-        #print(temp_at_coords_layers[:,i])
-        temp_layer = griddata(coords, temp_at_coords_layers[:,i], (interp_gridp.T, interp_gridl.T), method="nearest")
-        press_layer = griddata(coords, press_at_coords_layers[:,i], (interp_gridp.T, interp_gridl.T), method="nearest")
-        temp_model_3d[:,:,i] = temp_layer
-        press_model_3d[:,:,i] = press_layer
+   
     
 
     if True:
-        model, x, y, z = database.getNwAtEp(ep)
-        for i in range(0,len(x)):
-            for j in range(0,len(y)):
-                sta = Little_RStation(lat=x[i], lon=y[j], alt=0.0, source="BUTE GNSS-meteorology nrt procession", epoch=ep, fm_code="FM-116")
-                for k in range(0, len(z)):
+        
+        Nw_model, gridp_center, gridl_center, gridh_center = database.getNwAtEp(ep)
+
+
+        #fr = Epoch(np.array([2021,11,1,2,0,0]))
+        #to = Epoch(np.array([2021,11,1,3,0,0]))
+        writer = Little_RWriter(filename)
+        RAOB_stations = []
+        coords = np.empty((0,2))
+        temp_at_coords_layers = np.empty((0, len(gridh_center)))
+        press_at_coords_layers = np.empty((0, len(gridh_center)))
+        Nh_at_coords_layers = np.empty((0, len(gridh_center)))
+
+        for station in database.getStations("RAOB"):
+            RAOB_stations.append(station)
+
+            last_available = database.getLastRAOBSep(station.id, ep)
+
+            temp_profile = database.getRAOBSTempAtep(station_id=station.id, ep=last_available)
+            press_profile = database.getRAOBSPressAtep(station_id=station.id, ep=last_available)
+            Nh_profile = database.getRAOBSNdryAtep(station_id=station.id, ep=last_available)
+            
+            temp_layered_profile = np.append([gridh_center], [np.interp(gridh_center, temp_profile[:,0], temp_profile[:,1])], axis=0).T
+            press_layered_profile = np.append([gridh_center], [np.interp(gridh_center, press_profile[:,0], press_profile[:,1])], axis=0).T
+            Nh_layered_profile = np.append([gridh_center], [np.interp(gridh_center, Nh_profile[:,0], Nh_profile[:,1])], axis=0).T
+
+            coords = np.append(coords, station.getPLH()[0:2,:].T, axis=0)
+            temp_at_coords_layer = np.empty((len(gridh_center), ))
+            press_at_coords_layer = np.empty((len(gridh_center), ))
+            Nh_at_coords_layer = np.empty((len(gridh_center), ))
+            for i in range(0, len(gridh_center)):
+                temp_at_coords_layer[i] = temp_layered_profile[i,1]
+                press_at_coords_layer[i] = press_layered_profile[i,1]
+                Nh_at_coords_layer[i] = Nh_layered_profile[i,1]
+
+            temp_at_coords_layers = np.append(temp_at_coords_layers, [temp_at_coords_layer], axis=0)
+            press_at_coords_layers = np.append(press_at_coords_layers, [press_at_coords_layer], axis=0)
+            Nh_at_coords_layers = np.append(Nh_at_coords_layers, [Nh_at_coords_layer], axis=0)
+
+        interp_gridp, interp_gridl =  np.meshgrid(gridp_center, gridl_center)
+        temp_model_3d = np.empty((len(gridp_center), len(gridl_center), len(gridh_center)))
+        press_model_3d = np.empty((len(gridp_center), len(gridl_center), len(gridh_center)))
+        Nh_model_3d = np.empty((len(gridp_center), len(gridl_center), len(gridh_center)))
+
+        for i in range(0, np.shape(temp_at_coords_layers)[1]):
+            temp_layer = griddata(coords, temp_at_coords_layers[:,i], (interp_gridp.T, interp_gridl.T), method="nearest")
+            press_layer = griddata(coords, press_at_coords_layers[:,i], (interp_gridp.T, interp_gridl.T), method="nearest")
+            Nh_layer = griddata(coords, Nh_at_coords_layers[:,i], (interp_gridp.T, interp_gridl.T), method="nearest")
+            temp_model_3d[:,:,i] = temp_layer
+            press_model_3d[:,:,i] = press_layer
+            Nh_model_3d[:,:,i] = Nh_layer
+
+        for i in range(0,len(gridp_center)):
+            for j in range(0,len(gridl_center)):
+                sta = Little_RStation(lat=gridp_center[i], lon=gridl_center[j], alt=0.0, source="BUTE GNSS-meteorology nrt procession", epoch=ep, fm_code="FM-116")
+                for k in range(0, len(gridh_center)):
                     
-                    sta.add_data(P=np.array([press_model_3d[i,j,k], 0]), H=np.array([z[k], 0]), T=np.array([temp_model_3d[i,j,k], 0]), Td=np.array([model[i, j, k], 0]))
+                    sta.add_data(P=np.array([press_model_3d[i,j,k], 0]), H=np.array([gridh_center[k], 0]), T=np.array([temp_model_3d[i,j,k], 0]), Td=np.array([Nw_model[i, j, k] + Nh_model_3d[i, j, k], 0]))
                 writer.addStation(sta)
         writer.write()
         #print(model)
--- src/readdb.py
+++ src/readdb.py
@@ -120,6 +120,36 @@ class ReadDB(object):
             res = np.append(res, [[s[0], s[1]]], axis=0)
 
         return res
+    
+    def getRAOBSNdryAtep(self, station_id, ep):
+
+        sql = "SELECT HEIGHT, N_DRY FROM RAOBSREFR WHERE DATE='"+ep.date()+"' AND TIME='"+ep.time()+"' AND WMOID="+ str(station_id) +" ORDER BY HEIGHT ASC"
+
+        dbcursor = self._database.cursor()
+
+        dbcursor.execute(sql)
+
+        res = np.empty((0,2))
+        for s in dbcursor.fetchall():
+
+            res = np.append(res, [[s[0], s[1]]], axis=0)
+
+        return res
+
+    def getRAOBSNwetAtep(self, station_id, ep):
+
+        sql = "SELECT HEIGHT, N_WET FROM RAOBSREFR WHERE DATE='"+ep.date()+"' AND TIME='"+ep.time()+"' AND WMOID="+ str(station_id) +" ORDER BY HEIGHT ASC"
+
+        dbcursor = self._database.cursor()
+
+        dbcursor.execute(sql)
+
+        res = np.empty((0,2))
+        for s in dbcursor.fetchall():
+
+            res = np.append(res, [[s[0], s[1]]], axis=0)
+
+        return res
 
     def getLastRAOBSep(self, station_id, to=None):
         if to is None:
